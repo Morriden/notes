26e38dca44be321a6ce3cd5aeafa959bb7270e22
Erik Ford <ekford18@gmail.com>
2020-06-01 16:20:37 -0700

    completed test for add

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ console.log(minimist(process.argv.slice(2)));
 //valid functions. If there is an --add (return true)
 
 //if its true we do a parse.
+
+//bring execute, valid, and parse functions together.
+// use parse to create and object from process.argv.
+//use valid to check if it is a valid action.
+//if valid execute
+//IF NOT VALID show error.
