aa11addc84eede567708ac9f6887f1e350a71e6c
Erik Ford <ekford18@gmail.com>
2020-06-01 16:52:34 -0700

    all tests and functions completed for day one

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+const { parse, isValid } = require('./lib/input');
+const { execute } = require('./lib/notes');
 const minimist = require('minimist');
 
 console.log(minimist(process.argv.slice(2)));
-
-//valid functions. If there is an --add (return true)
-
-//if its true we do a parse.
 
 //bring execute, valid, and parse functions together.
 // use parse to create and object from process.argv.
 //use valid to check if it is a valid action.
 //if valid execute
 //IF NOT VALID show error.
+
+
+
+let parsed = parse(process.argv) ;
+
+if (isValid(parsed) === true) {
+    execute();
+} else {
+    console.log('failure to start');
+}
+
