32964f94846e7b941910253ffaf88231045df00e
Erik Ford <ekford18@gmail.com>
2020-06-03 13:23:54 -0700

    installed dependenices first test written

--- something.test.js
+++ something.test.js
@@ -1,11 +1,37 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+const  = require('./House');
 
+const mongodb = new MongoMemoryServer();
+
+describe('Note model', () => {
+    beforeAll(() => {
+      return mongodb.getUri()
+        .then(uri => mongoose.connect(uri, {
+          useUnifiedTopology: true,
+          useNewUrlParser: true
+        }));
+    });
+
+    beforeEach(() => {
+        return mongoose.connection.dropDatabase();
+    });
+
+    afterAll(() => {
+        return mongoose.connection.close()
+            .then(() => mongodb.stop())
+    });
 
 it('can build a note', () => {
-    return Notes.execute('note');
+    return Notes.execute('note')
         .then(house => {
             expect(house).toEqual({
                 _id: expect.anything(),
                 text: 'some text'
             })
         })
+})
+
+
+
 })
