edee630d60d07a451507d05994a1e62584b881b7
Erik Ford <ekford18@gmail.com>
2020-06-04 09:12:31 -0700

    corrected small comments

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const mongoose = require('mongoose');
 const Input = require('./Input');
-const Notes = require('./lib/Notes');
+const Notes = require('./lib/Note');
+
+mongoose.connect('mongodb://localhost:27017/notes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const input = new Input(process.argv);
 const isValidInput = input.isValid();
